83d500d2b2e982c2900a0e366f4291c30e87f784
mathislajs <mathislajs@gmail.com>
2025-03-23 00:26:55 -0400

    stuff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { ShewenyClient } from "sheweny";
-import { Partials, ActivityType, GatewayIntentBits } from "discord.js";
+import { Partials, ActivityType, GatewayIntentBits, TextChannel } from "discord.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -45,6 +45,21 @@ class Client extends ShewenyClient {
         content: `⚠️ You're executing this command too fast! You can run it again <t:${Math.floor(Date.now() / 1000) + 10}:R>.`, 
         ephemeral: true 
       });
+    });
+
+    this.on('ready', async () => {
+      console.log('Ready & online');
+      
+      try {
+        const channel = await this.channels.fetch('1226685483824713738') as TextChannel;
+        if (channel?.isTextBased()) {
+          await channel.send({
+            content: 'Restarted & now online',
+          });
+        }
+      } catch (error) {
+        console.error('Failed to send startup message:', error);
+      }
     });
 
     this.login(process.env.DISCORD_TOKEN);
